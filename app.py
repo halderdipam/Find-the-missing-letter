eb18fd81ad210a3ea32ccfae71257039fa68b3f5
Dipam Halder <43072215+halderdipam@users.noreply.github.com>
2021-02-08 21:21:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+# Solution One:
+def find_missing_letter(chars):
+    s = ''
+    for i in range(0,len(chars)-1):
+        if(ord(chars[i+1]) - ord(chars[i]) > 1):
+            s = chr(ord(chars[i])+1)
+
+    return s
+ 
+Solution Two:
+def find_missing_letter(chars):
+    s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    c= 0
+    f = False
+    for i in s:
+        if i == chars[c]:
+            f=True
+            c += 1
+
+    return i
+    
+print(find_missing_letter(['a','b','c','d','f']))
+    
+print(find_missing_letter(['a','b','c','d','f']))
